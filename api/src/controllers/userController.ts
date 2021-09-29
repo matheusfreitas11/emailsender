95d56e126a9f314d5b41df5f1706a313e13a53a6
Matheus Freitas <amizademt@hotmail.com>
2021-09-29 18:49:23 -0300

    Commit final

---
+++ api/src/controllers/userController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
+import { AppError } from "../errors/AppError";
+
+class userController {
+  async create(request: Request, response: Response) {
+    const {name, email} = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required()
+    });
+
+    try {
+      await schema.validate(request.body, {abortEarly: false});
+    } catch (err) {
+      throw new AppError(err);
+    };
+    
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    // função findOne: SELECT 8 FROM USERS WHERE EMAIL = "EMAIL"
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if(userAlreadyExists) {
+      throw new AppError("User already exists!");
+    };
+
+    const user = usersRepository.create({
+      name, 
+      email,
+    });
+
+    await usersRepository.save(user);
+    
+    return response.status(201).json(user);
+
+
+  }
+};
+
+export { userController };
